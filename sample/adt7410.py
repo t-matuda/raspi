b45c1cbaf7bd5d8a34d3550696c99dbd5f7dc92c
toshihiro matsuda <toshihiro.matsuda@gmail.com>
2017-03-20 02:54:52 +0000

    add sample program to get temperature using adt7410

---
+++ sample/adt7410.py
@@ -0,0 +1,13 @@
+import smbus
+import time
+
+i2c = smbus.SMBus(1)
+address = 0x48
+
+while True:
+    block = i2c.read_i2c_block_data(address, 0x00, 12)
+    temp = (block[0] << 8 | block[1]) >> 3
+    if(temp >= 4096):
+        temp -= 8192
+    print("Temperature:%6.2f" % (temp / 16.0))
+    time.sleep(1)
